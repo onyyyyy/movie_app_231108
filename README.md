4374783925cebef6a0a94a653f33c0852adad2e0
onyyyyy <hongki365@gmail.com>
2023-11-09 16:59:24 +0900

    231109 헤더 스타일 지정

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## 설치항목
 
-[] styled-components
+[X] styled-components
 [X] react-router-dom
-[] style.reset
+[X] styled-reset
 [] react-helmet
 [] font-awesome
 [] react-hook-form
@@ -10,7 +10,7 @@
 ## TODO
 
 [X] router 설정
-[] 각 component 제작
+[X] 각 component 제작
 [] api 설정
 [] Home 컴포넌트 구성
 [] Detail 컴포넌트 구성
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,40 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { routes } from "../routes";
+import { GlobalStyled, mainColors } from "../style/GlobalStyled";
+
+const Sheader = styled.div`
+  padding: 20px 5%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Logo = styled.div`
+  font-size: 24px;
+  font-weight: 700;
+`;
+const Menu = styled.ul`
+  display: flex;
+  align-items: center;
+  li {
+    margin-left: 50px;
+  }
+`;
+
 export const Header = () => {
-  return <div></div>;
+  return (
+    <Sheader>
+      <Logo>
+        <Link to={routes.home}>MOVIE</Link>
+      </Logo>
+      <Menu>
+        <li>
+          <Link to={routes.home}>Home</Link>
+        </li>
+        <li>
+          <Link to={routes.search}>Search</Link>
+        </li>
+      </Menu>
+    </Sheader>
+  );
 };
